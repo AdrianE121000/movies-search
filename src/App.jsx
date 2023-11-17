bd62966c1d69869192d9cef885ee3e9d89988aa6
AdrianE121000 <paracosa3s@gmail.com>
2023-11-17 10:18:56 -0500

    requerimientos terminados

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { Movies } from './components/Movies';
+import { useMovies } from './hooks/useMovies';
+import './App.css';
+
+function useSearch() {
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState('');
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return;
+    }
+
+    if (search === '') {
+      setError('No se puede buscar pelicula');
+      return;
+    }
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número');
+      return;
+    }
+    if (search.length < 3) {
+      setError('La busqueda deve tener al menos 3 caracteres');
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, setSearch, error };
+}
+
+function App() {
+  const [sort, setSort] = useState(false);
+  const { search, setSearch, error } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ search, sort });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    getMovies({ search });
+  };
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSort = () => {
+    setSort(!sort);
+  };
+
+  return (
+    <>
+      <div className='page'>
+        <header>
+          <h1>Buscador de peliculas</h1>
+          <form
+            className='form'
+            onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              value={search}
+              name='search'
+              placeholder='Avengers, Star Wars, The Matrix...'
+            />
+            <input
+              type='checkbox'
+              onChange={handleSort}
+              checked={sort}
+            />
+            <button type='submit'>Buscar</button>
+          </form>
+          {error && <p>{error}</p>}
+        </header>
+        <main>
+          {loading ? (
+            <div className='spinner'></div>
+          ) : (
+            <Movies movies={movies} />
+          )}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useMovies.js
@@ -0,0 +1,30 @@
+import { useRef, useState, useMemo, useCallback } from 'react';
+import { searchMovies } from '../servises/movies';
+
+export function useMovies({ search, sort }) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
+
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return;
+    try {
+      setLoading(true);
+      setError(null);
+      previousSearch.current = search;
+      const newMmovies = await searchMovies({ search });
+      setMovies(newMmovies);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const sortedMovies = useMemo(() => {
+    sort ? [...movies].sort((a, b) => a.title.localCompare(b.title)) : movies;
+  }, [sort, movies]);
+
+  return { movies: sortedMovies, getMovies, loading };
+}
